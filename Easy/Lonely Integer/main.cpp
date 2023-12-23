b12abd4ff2a4f6a5725646987d00b73c8fc954f8
Vincent <vincent01sj@gmail.com>
2023-12-24 00:19:17 +0700

    Lonely Integer Done

---
+++ Easy/Lonely Integer/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int lonelyList(vector<int> listValue){
+
+    sort(listValue.begin(), listValue.end());
+
+    for(int i=0 ; i<listValue.size() ; i+=2){
+        if(listValue[i] != listValue[i+1]){
+            return listValue[i];
+        }
+    }
+
+    return listValue[i-1];
+
+}
+
+
+int main(){
+
+    int n; 
+    vector<int> listValue;
+    
+    cin >> n; 
+
+    for(int i=0 ; i<n ; i++){
+        int value; 
+        cin >> value; 
+
+        listValue.push_back(value);
+    }
+
+    int result = lonelyList(listValue);
+
+    cout << result << endl;
+
+
+
+    return 0;
+}
